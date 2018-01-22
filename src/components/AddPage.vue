7d9f8716c3ee1e1f19f18c92584be140a503afd4
BMellet <benzero.m@gmail.com>
2018-01-22 11:48:26 +0100

    router added

---
+++ src/components/AddPage.vue
@@ -0,0 +1,5 @@
+<template>
+<v-app>
+  <p>ok</p>  
+</v-app>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Add from './components/AddPage'
+import VeilleListe from './components/VeilleListe'
 import Vuetify from 'vuetify'
 import('../node_modules/vuetify/dist/vuetify.min.css')
  
 Vue.use(Vuetify)
 Vue.config.productionTip = false
+const NotFound = { template: '<p>Page not found</p>' };
+const routes = {
+  '/': App,
+  '/ajouter': Add,
+  '/test':VeilleListe
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
 })
