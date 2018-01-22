fe82cf6ed71111f2472d3adc561e95cb1b1fa862
echayotte <echayotte@gmail.com>
2018-01-22 11:23:07 +0100

    import de vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import Materials from "vue-materials"
+// Vue.use(Materials)
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+
+import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.config.productionTip = false
 
